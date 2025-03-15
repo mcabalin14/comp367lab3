3a70ded2213c2bbfeaa3c6f9f78131076436e9c9
Michael Angelo Cabalinan <mcabalin@my.centennialcollege.ca>
2025-03-14 23:03:16 -0400

    Initial commit

---
+++ MvCMovie/MvCMovie/Data/MvCMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvCMovie.Data
+{
+    public class MvCMovieContext : DbContext
+    {
+        public MvCMovieContext (DbContextOptions<MvCMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
